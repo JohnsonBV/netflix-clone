ce657f54b3867cdf8dd8f929eb0b7d4aa43b2c3e
JohnsonBV <100324974+JohnsonBV@users.noreply.github.com>
2025-08-30 13:00:45 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { Outlet } from 'react-router-dom'
-import NavBar from './components/NavBar.jsx'
+import React from "react";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
+import "./App.css";
 
-export default function App() {
+function App() {
   return (
-    <>
-      <NavBar />
-      <Outlet />
-    </>
-  )
+    <div className="app">
+      {/* Navbar */}
+      <Nav />
+
+      {/* Banner */}
+      <Banner />
+
+      {/* Rows */}
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
 }
+
+export default App;
