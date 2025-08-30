0e95de7443eb8c9b6bb31994b0a73215daefd2d3
JohnsonBV <100324974+JohnsonBV@users.noreply.github.com>
2025-08-30 16:37:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
-})
+  resolve: {
+    alias: {
+      "@": "/src", // Optional: allows imports like "@/components/Row.jsx"
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+});
