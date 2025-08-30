f9c40f04bcb43be787446e5f5a7525e7dbcf1c45
JohnsonBV <100324974+JohnsonBV@users.noreply.github.com>
2025-08-30 15:06:49 -0400

    Update Row.jsx

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "./axios.js"; // custom axios baseURL setup
+import axios from "./axios"; // custom axios baseURL setup
 import "./Row.css";
 
 function Row({ title, fetchUrl, isLargeRow }) {
