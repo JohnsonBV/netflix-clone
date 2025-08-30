e747b102ade204b5961a7af3847c3c1a4e4bf549
JohnsonBV <100324974+JohnsonBV@users.noreply.github.com>
2025-08-30 19:29:31 -0400

    Update Row.jsx

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,46 +1,41 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../api/axios";
+import MovieDetail from "./MovieDetail";
 import "./Row.css";
-import { useMovie } from "./MovieContext.jsx";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  const { setSelectedMovie } = useMovie();
-  const base_url = "https://image.tmdb.org/t/p/original/";
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      try {
-        const request = await axios.get(fetchUrl);
-        setMovies(request.data.results);
-      } catch (error) {
-        console.error("Error fetching movies:", error);
-      }
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     }
     fetchData();
   }, [fetchUrl]);
 
-  const handleMovieClick = (movie) => {
-    setSelectedMovie(movie);
-  };
-
   return (
     <div className="row">
-      <h2 className="row__title">{title}</h2>
+      <h2>{title}</h2>
       <div className="row__posters">
-        {movies.map(
-          (movie) =>
-            movie.poster_path && (
-              <img
-                key={movie.id}
-                className="row__poster"
-                src={`${base_url}${movie.poster_path}`}
-                alt={movie.name || movie.title}
-                onClick={() => handleMovieClick(movie)}
-              />
-            )
-        )}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => setSelectedMovie(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`https://image.tmdb.org/t/p/w500${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
       </div>
+
+      {selectedMovie && (
+        <MovieDetail movie={selectedMovie} onClose={() => setSelectedMovie(null)} />
+      )}
     </div>
   );
 }
