7e3f9a5f2766f9c5a7a0a2c63df2c65a73393938
JohnsonBV <100324974+JohnsonBV@users.noreply.github.com>
2025-08-30 19:42:39 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [], // leave empty unless you need to exclude modules
-    },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      // Fallbacks for Node core modules (needed by some npm libs like movie-trailer)
+      util: "util/",
+      stream: "stream-browserify",
+      path: "path-browserify"
+    }
   },
+  define: {
+    "process.env": {}
+  }
 });
