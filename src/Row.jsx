57462393ec803af214e054715b267ea2b8b8ff89
JohnsonBV <100324974+JohnsonBV@users.noreply.github.com>
2025-08-30 20:20:59 -0400

    Update Row.jsx

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
-import axios from "./api/axios";
-import requests from "./requests";
+import axios from "./requests"; // your API fetcher
+import MovieDetail from "./MovieDetail";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -15,17 +19,21 @@ function Row({ title, fetchUrl }) {
 
   return (
     <div className="row">
-      <h2>{title}</h2>
+      <h2 className="row__title">{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => setSelectedMovie(movie)}
             className="row__poster"
-            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            src={`${base_url}${movie.poster_path}`}
             alt={movie.name || movie.title}
           />
         ))}
       </div>
+      {selectedMovie && (
+        <MovieDetail movie={selectedMovie} />
+      )}
     </div>
   );
 }
