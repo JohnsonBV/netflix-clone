9436a2a0f2927e2b8d800b2f31c63ec5ae19945a
JohnsonBV <100324974+JohnsonBV@users.noreply.github.com>
2025-09-21 11:32:11 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Row from "./Row";
+import Banner from "./Banner";
 import requests from "./requests";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      {/* Hero Banner */}
+      <Banner />
+
+      {/* Example rows */}
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
@@ -12,6 +18,7 @@ function App() {
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
