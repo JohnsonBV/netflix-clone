d82c7c83d203cc1171180e877a7374beeb428c23
JohnsonBV <100324974+JohnsonBV@users.noreply.github.com>
2025-10-05 11:56:59 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-      // Fallbacks for Node core modules (needed by some npm libs like movie-trailer)
-      util: "util/",
-      stream: "stream-browserify",
-      path: "path-browserify"
-    }
-  },
-  define: {
-    "process.env": {}
-  }
+  base: '/netflix-clone/', // 👈 must match your GitHub repo name
 });
