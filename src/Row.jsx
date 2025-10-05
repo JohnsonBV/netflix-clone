d129ee0a833cf704d7b6906ecbc1c6df9bdf5aa0
JohnsonBV <100324974+JohnsonBV@users.noreply.github.com>
2025-10-05 12:55:29 -0400

    Update Row.jsx

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
-import axios from "./axios";
+import React, { useState, useEffect } from "react";
 import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
+import axios from "../api/axios";
 import "./Row.css";
 
-const base_url = "https://image.tmdb.org/t/p/original/";
-
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
     async function fetchData() {
@@ -19,19 +19,11 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
-  const opts = {
-    height: "390",
-    width: "100%",
-    playerVars: {
-      autoplay: 1,
-    },
-  };
-
   const handleClick = (movie) => {
     if (trailerUrl) {
-      setTrailerUrl(""); // close trailer if already open
+      setTrailerUrl("");
     } else {
-      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+      movieTrailer(movie?.name || movie?.title || "")
         .then((url) => {
           const urlParams = new URLSearchParams(new URL(url).search);
           setTrailerUrl(urlParams.get("v"));
@@ -40,16 +32,26 @@ function Row({ title, fetchUrl }) {
     }
   };
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className="row-posters">
+      <div className="row__posters">
         {movies.map((movie) => (
           <img
             key={movie.id}
             onClick={() => handleClick(movie)}
-            className="row-poster"
-            src={`${base_url}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
